96a488b15f6ada8bc724447e4a4e084f59a2613a
Igor Kuznetsov <kuznetsov.public@yandex.ru>
2023-02-20 21:59:39 +0300

    Задача #2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,30 @@
 public class Main {
   public static void main(String[] args) {
-    fullCycle(6,6, "*");
+//    fullCycle(6,6, "*");
+    zeroCycle(6,6, "*", "0");
   }
 
   public static void fullCycle(int numberFirst, int numberSecond, String symbol) {
     for (int i = 0; i < numberFirst; i++) {
       for (int j = 0; j < numberSecond; j++) {
         System.out.print(symbol + " ");
+        if (j == numberSecond - 1) {
+          System.out.println("");
+        }
+      }
+    }
+  }
+
+  public static void zeroCycle(int numberFirst, int numberSecond, String symbolFirst, String symbolSecond) {
+    for (int i = 0; i < numberFirst; i++) {
+      for (int j = 0; j < numberSecond; j++) {
+        if(i == 0 || i == numberFirst - 1) {
+          System.out.print(symbolSecond + " ");
+        }else if (j == 0 || j == numberSecond - 1) {
+          System.out.print(symbolSecond + " ");
+        }else {
+          System.out.print(symbolFirst + " ");
+        }
         if (j == numberSecond - 1) {
           System.out.println("");
         }
