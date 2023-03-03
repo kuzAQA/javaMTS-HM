f22e666a1467adb7439b3cc2f8eb54377650e2de
Igor Kuznetsov <kuznetsov.public@yandex.ru>
2023-03-03 12:28:36 +0300

    Домашняя работа массивам #1

---
+++ src/main/java/ArraysHomework/ArraysHomework.java
@@ -0,0 +1,17 @@
+package ArraysHomework;
+
+import java.util.Arrays;
+
+public class ArraysHomework {
+
+    static int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+    public static void multiArr() {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,17 @@
-import static CycleHomework.CycleHomework.triangleCycle;
+import static ArraysHomework.ArraysHomework.multiArr;
 
-import ExtendsHomework.Task_1.Animal;
-import ExtendsHomework.Task_1.Bird;
-import ExtendsHomework.Task_1.Cat;
-import ExtendsHomework.Task_1.Dog;
-import ExtendsHomework.Task_1.Horse;
+import ArraysHomework.ArraysHomework;
+import ExtendsHomework.Task_2.Cat;
 import ExtendsHomework.Task_2.Human;
 import ExtendsHomework.Task_2.Treadmill;
 import ExtendsHomework.Task_2.Trial;
-import ExtendsHomework.Task_2.Wall;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
 /*        Домашняя работа по циклам
         fullCycle(6,6, "*");
@@ -19,7 +19,7 @@ public class Main {
         triangleCycle(6,6, "*", "0");*/
 
 //        Домашняя работа по наследованию классов #1
-        Animal dog = new Dog();
+/*      Animal dog = new Dog();
         Animal cat = new Cat();
         Animal horse = new Horse();
         Animal bird = new Bird();
@@ -29,10 +29,13 @@ public class Main {
         for (Animal animal : animals) {
             animal.run(200);
             animal.swim(2);
-            animal.jumpingOverObstacles(2);
-        }
+            animal.jumpingOverObstacles(2);*/
 
 //        Домашняя работа по наследованию классов #2
 
+//        Домашняя работа массивам #1
+        multiArr();
+
+//        Домашняя работа массивам #2
     }
 }
