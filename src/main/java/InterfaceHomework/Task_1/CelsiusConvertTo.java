e7922123679e821856801932a30c7881ee126b2c
Igor Kuznetsov <kuznetsov.public@yandex.ru>
2023-03-20 00:43:28 +0300

    Рефактор домашки по интерфейсам 1. Конвертация в цельсии

--- src/main/java/InterfaceHomework/Task_1/CelsiusConvertTo.java
+++ src/main/java/InterfaceHomework/Task_1/CelsiusConvertTo.java
@@ -2,12 +2,5 @@ package InterfaceHomework.Task_1;
 
 public interface CelsiusConvertTo {
 
-/*    static double celsiusToKelvin1 (double celsius) {
-        double CONST = 274.15;
-        return celsius + CONST;
-    }*/
-
-    double celsiusToKelvin(double celsius);
-
-    double celsiusToFahrenheit(double celsius);
+    double celsiusTo(double celsius, String scale);
 }
--- src/main/java/InterfaceHomework/Task_1/Main.java
+++ src/main/java/InterfaceHomework/Task_1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         CelsiusConvertTo convert = new CelsiusConvert();
-        System.out.println(convert.celsiusToFahrenheit(12));
-        System.out.println(convert.celsiusToKelvin(12));
+        System.out.println(convert.celsiusTo(12, "K"));
+        System.out.println(convert.celsiusTo(12, "F"));
     }
 }
